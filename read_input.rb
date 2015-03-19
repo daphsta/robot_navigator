ab4bd85c56dec6c4a239fe5b965c348753d4599d
drouw <drouw@localsearch.com.au>
2015-03-20 08:28:19 +1000

    removed instruction factory

--- read_input.rb
+++ read_input.rb
@@ -1,16 +1,10 @@
 require_relative 'robot'
-require_relative 'factories/instruction_factory'
 
 class ReadInput
 
   class << self
     def instruction_from_file
       begin
-        #Stdin while stdin get as string
-        #pipe in
-        #object send method_missing
-        #robot.send(instruction.to_sym)
-
         robot = Robot.new
         while(instruction = STDIN.gets)
           inst = instruction.split(' ')
