034cc0f54550543eef6dbcf149a6089e49358455
drouw <drouw@localsearch.com.au>
2015-04-14 08:04:52 +1000

    Used minitest spec instead

--- robot_test.rb
+++ robot_test.rb
@@ -1,76 +1,69 @@
 require "minitest/autorun"
-require_relative "read_input"
+require_relative "robot"
 
+describe Robot do
 
-class RobotTest < Minitest::Test
-  def test_validate_out_of_grid
-    inst = "4,6,NORTH"
+  describe 'MOVE instruction' do
     robot = Robot.new(6,6)
-    robot.place(inst)
-    robot.move
 
-    assert_equal(false,robot.valid)
+    describe 'with valid parameters' do
+      before do
+        robot.place("2,2,WEST")
+      end
+
+      it 'must move' do
+        robot.move
+        robot.current_position.x.must_equal 1
+      end
+
+      it 'must be valid' do
+        robot.move
+        robot.valid.must_equal true
+      end
+    end
+
+    describe 'with invalid parameters' do
+      before do
+        robot.place("-6,-6,NORTH")
+      end
+
+      it 'must not be valid' do
+        robot.move
+        robot.valid.must_equal false
+      end
+    end
+
+    describe 'out of grid instructions' do
+      it 'ignores move instruction beyond y axis' do
+        robot.place("4,6,NORTH")
+        robot.move
+        robot.current_position.y.must_equal 6
+      end
+
+      it 'ignores move instruction beyond x axis' do
+        robot.place("0,4,SOUTH")
+        robot.move
+        robot.current_position.x.must_equal 0
+      end
+    end
+
+    describe 'moves robot' do
+      it 'moves into correct position and direction' do
+        robot.place("1,2,EAST")
+        robot.move
+        robot.move
+        robot.left
+
+        robot.current_direction.must_equal "NORTH"
+        robot.current_position.x.must_equal 3
+      end
+    end
   end
 
-  def test_validate_negative_coordinate
-    inst = "-6,-6,NORTH"
-    robot = Robot.new(6,6)
-    robot.place(inst)
-    robot.move
+  describe 'PLACE instruction' do
+    describe 'valid parameters' do
 
-    assert_equal(false,robot.valid)
-  end
-
-  def test_ignore_instructions_if_out_of_grid
-    inst = "5,5,NORTH"
-    robot = Robot.new(6,6)
-    robot.place(inst)
-    robot.move
-
-    assert_equal(5,robot.current_position.x)
-    assert_equal(5,robot.current_position.y)
-  end
-
-
-  def test_robot_movements
-    robot = Robot.new(6,6)
-    robot.place("1,2,EAST")
-    robot.move
-    robot.move
-    robot.left
-
-
-    assert_equal("NORTH",robot.current_direction)
-    assert_equal(3,robot.current_position.x)
-  end
-
-  def test_should_not_go_out_of_x_plane
-    robot = Robot.new(6,6)
-    robot.place("2,2,WEST")
-    robot.move
-    robot.left
-    robot.right
-    robot.move
-    robot.move
-    robot.right
-
-    assert_equal("NORTH",robot.current_direction)
-    assert_equal(0,robot.current_position.x)
-  end
-
-  def test_should_not_go_out_of_y_plane
-    robot = Robot.new(6,6)
-    robot.place("2,3,NORTH")
-    robot.move
-    robot.left
-    robot.right
-    robot.move
-    robot.move
-    robot.move
-    robot.right
-
-    assert_equal("EAST",robot.current_direction)
-    assert_equal(5,robot.current_position.y)
+    end
   end
 
 end
